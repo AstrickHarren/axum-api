10edb2b3b3c3153209a0ef552aa83e5735851c29
Astrick <astrick.harren.at.work@gmail.com>
2025-10-16 20:38:24 +0800

    refactor: path extractor

--- src/extractors/path.rs
+++ src/extractors/path.rs
@@ -1,20 +1,24 @@
 use {
-    crate::api_error::ApiError, aide::OperationInput, axum::extract::FromRequest,
+    crate::api_error::ApiError, aide::OperationInput, axum::extract::FromRequestParts,
     schemars::JsonSchema, serde::de::DeserializeOwned,
 };
 
 #[derive(Debug)]
 pub struct Path<T>(pub T);
 
-impl<S, T> FromRequest<S> for Path<T>
+impl<S, T> FromRequestParts<S> for Path<T>
 where
     T: DeserializeOwned + Send,
     S: Send + Sync,
 {
     type Rejection = ApiError;
 
-    async fn from_request(req: axum::extract::Request, state: &S) -> Result<Self, Self::Rejection> {
-        let axum::extract::Path(data) = axum::extract::Path::from_request(req, state).await?;
+    async fn from_request_parts(
+        parts: &mut axum::http::request::Parts,
+        state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        let axum::extract::Path(data) =
+            axum::extract::Path::from_request_parts(parts, state).await?;
         Ok(Path(data))
     }
 }
