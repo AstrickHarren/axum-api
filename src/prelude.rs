dcfe9d99d5a7c64c24e6a09688146b3ae2ddf9dd
Astrick <astrick.harren.at.work@gmail.com>
2025-10-16 17:59:27 +0800

    feat: add prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,10 +1,19 @@
 pub use {
-    crate::extractors::*,
+    crate::{
+        api_error::{ApiError, IntoApiError},
+        diesel_otel::RunQueryDsl,
+        extractors::*,
+    },
     aide::axum::{
-        ApiRouter,
+        ApiRouter, IntoApiResponse,
         routing::{
-            delete_with, get_with, head_with, options_with, patch_with, post_with, put_with,
-            trace_with,
+            ApiMethodRouter, delete_with, get_with, head_with, options_with, patch_with, post_with,
+            put_with, trace_with,
         },
     },
+    axum::http::StatusCode,
+    diesel::{ExpressionMethods, QueryDsl, prelude::*},
+    schemars::JsonSchema,
+    serde::{Deserialize, Serialize},
+    validator::Validate,
 };
