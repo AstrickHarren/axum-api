a3e23f469ef4cb9d5afb863ac4156ea55a68e99f
Astrick <astrick.harren.at.work@gmail.com>
2025-10-18 12:36:47 +0800

    refactor: api router ext

--- src/aide_ext.rs
+++ src/aide_ext.rs
@@ -1,15 +1,16 @@
 use aide::axum::ApiRouter;
-pub trait ApiRouterExt {
-    fn into_api_router(self) -> ApiRouter;
-    fn api_tag(self, tag: &'static str) -> ApiRouter
+pub trait ApiRouterExt<S: Clone + Sync + Send> {
+    fn into_api_router(self) -> ApiRouter<S>;
+    fn api_tag(self, tag: &'static str) -> ApiRouter<S>
     where
         Self: Sized,
+        S: 'static,
     {
         self.into_api_router().with_path_items(|t| t.tag(tag))
     }
 }
-impl ApiRouterExt for ApiRouter {
-    fn into_api_router(self) -> ApiRouter {
+impl<S: Clone + Sync + Send> ApiRouterExt<S> for ApiRouter<S> {
+    fn into_api_router(self) -> ApiRouter<S> {
         self
     }
 }
